b30a473953d35fe347c608990f2523027e529209
suhasKumar <suhassuhas3468@gmail.com>
2023-12-02 08:38:03 +0530

    border and title

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
-console.dir(document)
-console.log(document.all)
-console.log(document.title)
-console.log(document.all[6])
-// document.all[8].textContent = "Hello"
-console.log(document.forms)
+// console.dir(document)
+// console.log(document.all)
+// console.log(document.title)
+// console.log(document.all[6])
+// // document.all[8].textContent = "Hello"
+// console.log(document.forms)
 
-const headerTitle = document.getElementById('header-title')
-console.log(headerTitle)
-console.log(headerTitle.textContent = "Hello World ")
-console.log(headerTitle.innerHTML = "Hello World")
-console.log(headerTitle)
+// const headerTitle = document.getElementById('header-title')
+// console.log(headerTitle)
+// console.log(headerTitle.textContent = "Hello World ")
+// console.log(headerTitle.innerHTML = "Hello World")
+// console.log(headerTitle)
+
+var header = document.getElementById('main-header')
+header.style.border = 'solid 10px black'
+
+var title = document.getElementById('main-title')
+title.fontWeight='bold';

0cd4ec1423b3b0dd2f0617da38d0d4b1e4b7828f
suhasKumar <suhassuhas3468@gmail.com>
2023-12-02 08:15:06 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+console.dir(document)
+console.log(document.all)
+console.log(document.title)
+console.log(document.all[6])
+// document.all[8].textContent = "Hello"
+console.log(document.forms)
+
+const headerTitle = document.getElementById('header-title')
+console.log(headerTitle)
+console.log(headerTitle.textContent = "Hello World ")
+console.log(headerTitle.innerHTML = "Hello World")
+console.log(headerTitle)

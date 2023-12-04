d8e56894f05469231648e32d78afd4fe0532faa5
suhasKumar <suhassuhas3468@gmail.com>
2023-12-04 19:00:23 +0530

    Edit and delete users

--- main.js
+++ main.js
@@ -25,24 +25,49 @@ function onSubmit(e) {
         // Retrieve existing users from local storage
         const users = JSON.parse(localStorage.getItem('users')) || [];
 
-        // Create a new user object
-        const newUser = {
-            name: name.value,
-            email: email.value
-        };
+        // Check if editing or adding a new user
+        const editingUser = myForm.getAttribute('data-edit-id');
+        if (editingUser) {
+            // Editing an existing user
+            const editedUser = users.find(user => user.id === parseInt(editingUser));
+            if (editedUser) {
+                editedUser.name = name.value;
+                editedUser.email = email.value;
 
-        // Add the new user to the array of users
-        users.push(newUser);
+                // Update local storage with the modified user data
+                localStorage.setItem('users', JSON.stringify(users));
 
-        // Update local storage with the new array of users
-        localStorage.setItem('users', JSON.stringify(users));
+                // Update the UI with the modified user details
+                updateUserInUI(editedUser);
 
-        // Update the UI with the new user details and delete button
-        addUserToUI(newUser);
+                // Clear the editing flag
+                myForm.removeAttribute('data-edit-id');
 
-        // Clear input fields
-        name.value = '';
-        email.value = '';
+                // Clear input fields
+                name.value = '';
+                email.value = '';
+            }
+        } else {
+            // Create a new user object
+            const newUser = {
+                id: new Date().getTime(), // Unique identifier (using timestamp)
+                name: name.value,
+                email: email.value
+            };
+
+            // Add the new user to the array of users
+            users.push(newUser);
+
+            // Update local storage with the new array of users
+            localStorage.setItem('users', JSON.stringify(users));
+
+            // Update the UI with the new user details and delete/edit buttons
+            addUserToUI(newUser);
+
+            // Clear input fields
+            name.value = '';
+            email.value = '';
+        }
     }
 }
 
@@ -51,12 +76,38 @@ function addUserToUI(user) {
     li.appendChild(document.createTextNode(`${user.name}: ${user.email}`));
 
     // Add delete button
-    const btn = document.createElement('button');
-    btn.appendChild(document.createTextNode('Delete'));
-    btn.addEventListener('click', () => deleteUser(user));
-    li.appendChild(btn);
+    const deleteBtn = document.createElement('button');
+    deleteBtn.appendChild(document.createTextNode('Delete'));
+    deleteBtn.addEventListener('click', () => deleteUser(user));
+    li.appendChild(deleteBtn);
+
+    // Add edit button
+    const editBtn = document.createElement('button');
+    editBtn.appendChild(document.createTextNode('Edit'));
+    editBtn.addEventListener('click', () => editUser(user));
+    li.appendChild(editBtn);
 
     userList.appendChild(li);
+}
+
+function updateUserInUI(user) {
+    // Update the user in the UI
+    const userListItems = document.querySelectorAll('#users li');
+    userListItems.forEach(li => {
+        const textContent = li.textContent || li.innerText;
+        if (textContent.includes(`${user.name}: ${user.email}`)) {
+            li.textContent = `${user.name}: ${user.email}`;
+        }
+    });
+}
+
+function editUser(userToEdit) {
+    // Populate the form fields with user data
+    name.value = userToEdit.name;
+    email.value = userToEdit.email;
+
+    // Set the editing flag on the form
+    myForm.setAttribute('data-edit-id', userToEdit.id);
 }
 
 function deleteUser(userToDelete) {
@@ -72,8 +123,7 @@ function deleteUser(userToDelete) {
     // Remove the user from local storage
     const users = JSON.parse(localStorage.getItem('users')) || [];
     const updatedUsers = users.filter(user => (
-        user.name !== userToDelete.name || user.email !== userToDelete.email
+        user.id !== userToDelete.id
     ));
     localStorage.setItem('users', JSON.stringify(updatedUsers));
 }
-
